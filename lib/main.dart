e39b75896aa87e0056d9465562f1c70cf9708787
Penpicha Priwan <Penpicha.pri@gmail.com>
2026-01-19 10:59:40 +0700

    add very hate

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo love love',
-      title: 'Flutter Demo hate hate'
+      title: 'Flutter Demo hate hate2'
       theme: ThemeData(
         // This is the theme of your application.
         //
